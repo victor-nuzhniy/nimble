471826a13c1ac88f9e4a5752eccd6cd6c126427c
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-03 21:28:19 +0300

    added 'api' app to installed_app list, created first migrations

--- api/migrations/0001_create_contacts.py
+++ api/migrations/0001_create_contacts.py
@@ -17,8 +17,16 @@ class Migration(migrations.Migration):
             contact_id integer NOT NULL,
             first_name varchar(100) NOT NULL,
             last_name varchar(100) NOT NULL,
-            email varchar(100)
+            email varchar(100),
+            CONSTRAINT rector_pk PRIMARY KEY (contact_id)
             );
+            CREATE SEQUENCE IF NOT EXISTS contact_id_seq
+             AS bigint START WITH 1 INCREMENT BY 1;
+            ALTER TABLE contacts ALTER COLUMN contact_id
+             SET DEFAULT nextval('contact_id_seq');
+            """,
             """
+            DROP TABLE contacts;
+            """,
         )
     ]
