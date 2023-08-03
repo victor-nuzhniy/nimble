04bbe4c24d6f24a84a6a5400b83da9c4949783fc
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-03 22:57:53 +0300

    dev modified config.urls, created contact_json view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+"""Module with view funtion for 'api' app."""
+from django.db import connection
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from api.utils import dictfetchall
+
+
+@api_view(["GET"])
+def contacts_json(request):
+    """Get contacts list data."""
+    query = "SELECT * FROM contacts"
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        contacts = dictfetchall(cursor)
+    return Response({"contacts": contacts})
