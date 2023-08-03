16cfead2baf4e56e4a8c66de29747d5b94a6a701
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-04 00:28:11 +0300

    dev added validate_contact and get_insert_contact_query to api.utils

--- api/utils.py
+++ api/utils.py
@@ -1,10 +1,34 @@
 """Utility functional for 'api' app."""
+from typing import Dict
 
 
 def dictfetchall(cursor):
     """
     Return all rows from a cursor as a dict.
+
     Assume the column names are unique.
     """
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+
+def validate_contact(contact: Dict) -> bool:
+    """Validate contact."""
+    if any(
+        [
+            len(contact["first name"]) > 100,
+            len(contact["last name"]) > 100,
+            len(contact["Email"]) > 100,
+        ]
+    ):
+        return False
+    return True
+
+
+def get_insert_contact_query(contact: Dict) -> str:
+    """Get query string with SQL statement for creating contact."""
+    return (
+        f"INSERT INTO contacts(first_name, last_name, email) VALUES "
+        f"('{contact.get('first name')}', '{contact.get('last name')}', "
+        f"'{contact.get('Email')}');"
+    )
