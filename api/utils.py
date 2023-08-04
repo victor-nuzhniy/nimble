1d2f318fa994b1ed1ff38034ada365305b209428
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-04 17:07:30 +0300

    dev modified validate_contact and added validate_contact_input

--- api/utils.py
+++ api/utils.py
@@ -13,12 +13,25 @@ def dictfetchall(cursor):
 
 
 def validate_contact(contact: Dict) -> bool:
-    """Validate contact."""
+    """Validate contact from csv."""
     if any(
         [
-            len(contact["first name"]) > 100,
-            len(contact["last name"]) > 100,
-            len(contact["Email"]) > 100,
+            0 > len(contact.get("first name", "")) > 100,
+            0 > len(contact.get("last name", "")) > 100,
+            0 > len(contact.get("Email", "")) > 100,
+        ]
+    ):
+        return False
+    return True
+
+
+def validate_contact_input(contact: Dict) -> bool:
+    """Validate contact from api input."""
+    if any(
+        [
+            0 > len(contact.get("first_name", "")) > 100,
+            0 > len(contact.get("last_name", "")) > 100,
+            0 > len(contact.get("email", "")) > 100,
         ]
     ):
         return False
