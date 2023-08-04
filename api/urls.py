32a38c211b5c539247e4ab302ec2c3ff8f8bbb84
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-04 17:10:23 +0300

    dev added urls to created views

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,18 @@
 """Urls list for 'api' app."""
 from django.urls import path
 
-from api.views import delete_contacts, get_contacts
+from api.views import (
+    contact,
+    create_contact,
+    delete_contacts,
+    get_contacts,
+    update_contact,
+)
 
 urlpatterns = [
     path("contacts/", get_contacts),
     path("contacts/delete/", delete_contacts),
+    path("contact/create/", create_contact),
+    path("contact/<int:pk>/", contact),
+    path("contact/<int:pk>/update", update_contact),
 ]
