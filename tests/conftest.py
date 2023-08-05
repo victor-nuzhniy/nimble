39afd20dbb2f8e24a208e9cb3a024c1430e35301
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-05 20:38:13 +0300

    dev-test added fill_contacts_table_with_data fixture to tests/conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,25 @@
 import random
 
 import pytest
+from django.db import connection
+from faker import Faker
 
 
 @pytest.fixture(scope="function", autouse=True)
 def faker_seed() -> None:
     """Generate random seed for Faker instance."""
     return random.seed(version=3)
+
+
+@pytest.fixture
+def fill_contacts_table_with_data(faker: Faker) -> None:
+    """Fill test db with fake data."""
+    with connection.cursor() as cursor:
+        for _ in range(30):
+            query = (
+                f"INSERT INTO contacts (first_name, last_name, email) "
+                f"VALUES ('{faker.first_name()}', "
+                f"'{faker.last_name()}', "
+                f"'{faker.email()}');"
+            )
+            cursor.execute(query)
