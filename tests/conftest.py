069df795a6e4d9ffe5f0f486a57cd14125e74d52
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-05 16:05:34 +0300

    dev-test created init and conftest test module

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+"""Pytest fixtures for 'API' project."""
+
+import random
+
+import pytest
+
+
+@pytest.fixture(scope="function", autouse=True)
+def faker_seed() -> None:
+    """Generate random seed for Faker instance."""
+    return random.seed(version=3)
