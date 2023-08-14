9a84eff9e8a6edbe8293d520cf38f12f95a2ca8f
victro-nuzhniy <nuzhniyva@gmail.com>
2023-08-14 13:30:43 +0300

    dev-correct refactor queries, added params

--- api/queries.py
+++ api/queries.py
@@ -1,70 +1,75 @@
 """Module for SQL queries for 'api' app."""
-from typing import Dict
+from typing import Dict, Tuple
 
 
-def get_select_contacts_query():
+def get_select_contacts_query() -> Tuple[str, Tuple]:
     """Get query for retrievnig all contacts."""
-    return "SELECT * FROM contacts ORDER BY contact_id ASC;"
+    return "SELECT * FROM contacts ORDER BY contact_id ASC;", tuple()
 
 
-def get_search_contacts_query(search_data: str) -> str:
+def get_search_contacts_query(search_data: str) -> Tuple[str, Tuple]:
     """Get query for searching contacts with full text search."""
     return (
-        f"SELECT * FROM contacts WHERE make_tsvector(first_name, last_name,"
-        f" email) @@ websearch_to_tsquery('english', '{search_data}');"
+        "SELECT * FROM contacts WHERE make_tsvector(first_name, last_name,"
+        " email) @@ websearch_to_tsquery('english', %s);",
+        (search_data,),
     )
 
 
-def get_insert_contact_query(contact: Dict) -> str:
+def get_insert_contact_query(contact: Dict) -> Tuple[str, Tuple]:
     """Get query string with SQL statement for creating contact."""
     return (
-        f"INSERT INTO contacts(first_name, last_name, email) VALUES "
-        f"('{contact.get('first name')}', '{contact.get('last name')}', "
-        f"'{contact.get('Email')}');"
+        "INSERT INTO contacts(first_name, last_name, email) VALUES (%s, %s, %s);",
+        (contact.get("first name"), contact.get("last name"), contact.get("Email")),
     )
 
 
-def get_delete_contacts_query() -> str:
+def get_delete_contacts_query() -> Tuple[str, Tuple]:
     """Get query string with SQL statement for deleting contacts."""
-    return "DELETE FROM contacts;"
+    return "DELETE FROM contacts;", tuple()
 
 
-def get_read_single_contact_query(pk: int) -> str:
+def get_read_single_contact_query(pk: int) -> Tuple[str, Tuple]:
     """Get read query to retrieve single contact."""
-    return f"SELECT * FROM contacts WHERE contact_id = {pk};"
+    return "SELECT * FROM contacts WHERE contact_id = %s;", (pk,)
 
 
-def get_create_single_contact_query(first_name: str, last_name: str, email: str) -> str:
+def get_create_single_contact_query(
+    first_name: str, last_name: str, email: str
+) -> Tuple[str, Tuple]:
     """Get create query to create single contact."""
     return (
-        f"INSERT INTO contacts(first_name, last_name, email) VALUES"
-        f"('{first_name}', '{last_name}', '{email}');"
+        "INSERT INTO contacts(first_name, last_name, email) VALUES (%s, %s, %s);",
+        (first_name, last_name, email),
     )
 
 
 def get_update_single_contact_query(
     pk: int, first_name: str, last_name: str, email: str
-) -> str:
+) -> Tuple[str, Tuple]:
     """Get update query to update single contact."""
     return (
-        f"UPDATE contacts "
-        f"SET first_name = '{first_name}', last_name = '{last_name}', "
-        f"email = '{email}' WHERE contact_id = {pk};"
+        "UPDATE contacts "
+        "SET first_name = %s, last_name = %s, email = %s WHERE contact_id = %s;",
+        (first_name, last_name, email, pk),
     )
 
 
-def get_delete_single_contact_query(pk: int) -> str:
+def get_delete_single_contact_query(pk: int) -> Tuple[str, Tuple]:
     """Get delete query to delete single contact."""
-    return f"DELETE FROM contacts WHERE contact_id = {pk};"
+    return "DELETE FROM contacts WHERE contact_id = %s;", (pk,)
 
 
-def get_insert_with_update_contacts_query(contact: Dict) -> str:
+def get_insert_with_update_contacts_query(contact: Dict) -> Tuple[str, Tuple]:
     """Get insert query with on-conflict option with do update."""
     return (
-        f"INSERT INTO contacts (first_name, last_name, email) "
-        f"VALUES ('{contact.get('first_name', '')}', "
-        f"'{contact.get('last_name', '')}', "
-        f"'{contact.get('email', '')}') "
-        f"ON CONFLICT (first_name, last_name) "
-        f"DO UPDATE SET email = '{contact.get('email', '')}';"
+        "INSERT INTO contacts (first_name, last_name, email) "
+        "VALUES (%s, %s, %s) ON CONFLICT (first_name, last_name) "
+        "DO UPDATE SET email = %s;",
+        (
+            contact.get("first_name", ""),
+            contact.get("last_name", ""),
+            contact.get("email", ""),
+            contact.get("email", ""),
+        ),
     )
